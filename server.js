fb21cb2b1e4d41b38f675a8354adf960addf0cad
Matt Stevens <matt@stevens.net.nz>
2014-05-01 16:25:50 +1200

    added stuff while reading up on Heroku

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+
+//=====================================
+// cribbed from devcenter.heroku.com/articles/getting-started-with-nodejs
+// hooks up the postgres db
+//=====================================
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
+// end crib
+// ====================================
+
+
 app.use(logfmt.requestLogger());
 
 var spots = [
