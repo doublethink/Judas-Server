e36366135f91e47bba191ec1e5287e8614fb5442
Matt-- <matt@stevens.net.nz>
2014-05-09 09:37:54 +1200

    Added some code for posts, not tested yet...

--- server.js
+++ server.js
@@ -4,22 +4,15 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var pg = require('pg');
+// npm install body-parser
 var bodyParser = require('body-parser');
-
-var http = require('http'); // am backing off express for the moment
-
+var http = require('http'); // TODO remove?
 
 var app = express();
-//app.engine('html', require('ejs').renderfile);
-//app.set("view options", {layout: false});
-//app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views'); // TODO I think this is a default - remove?
 app.engine('html', require('ejs').renderFile);
 
 app.use(logfmt.requestLogger());
-
-// npm install body-parser
 app.use(bodyParser());
 
 //====================================
@@ -34,7 +27,6 @@ var pests = [
   {name : 'Possum', colour : 'grey', danger : 'eats trees', found : 'look in trees'},
   {name : 'Stoat', colour : 'black and white', danger : 'eats eggs', found : 'around tree bottoms'}
 ];
-
 // end dummy data
 
 //=====================================
@@ -64,18 +56,18 @@ app.get('/matt', function(req, res) {
   res.statusCode = 502;
   res.setHeader("Set-Cookie", ["type=ninja", "language=javascript"]);
 
-  //...node.js method, write, write then end. 
-  //...is not parsed as html text (set Content-Type = text/html)
+  ////...node.js method, write, write then end.
+  ////...is not parsed as html text (set Content-Type = text/html)
   //res.setHeader("Content-Type", "text/html");
   //res.write('Matt testing...<br>');
   //res.end('Last testing text.');
 
 
-  //...sends files. Filepath, relative is ./views or views, absolute is /views
-  //...parses at html (set Content-Type = text/html)
+  ////...sends files. Filepath, relative is ./views or views, absolute is /views
+  ////...parses at html (set Content-Type = text/html)
   res.sendfile('./views/test.html');
 
-  //...not working, hangs up
+  ////...not working, hangs up
   //res.render('./views/test.html', function(err, html){ 'I am a render.' });
   
   //res.send("Matt was here... bwahahaha");
@@ -84,22 +76,21 @@ app.get('/matt', function(req, res) {
 app.get('/test', function(req, res){
   //...sends text
   //...parses as html (Content-Type = text/html)
-  res.send('Server responds to \"test\".\"<br>');
+  res.send('Server responds to \"test\".<br>');
 });
 
 app.get('/test/:id', function(req, res){
   if(req.param('id') == 'test'){
     res.sendfile('./views/test.html');
   }else if(req.param('id') == 'page' ||
-      req.param('id') == 'testpage' ||
-      req.param('id') == 'testpage.html'
+           req.param('id') == 'testpage' ||
+           req.param('id') == 'testpage.html'
       ){
     res.sendfile('./views/testpage.html');
   }else{
     res.send('Sorry, don\'t recognise that command.<br>');
   }
 });
-
 
 app.get('/pests/:id', function(req, res){
   if(req.param('id') == 'possum'){
@@ -159,10 +150,41 @@ app.post('/pestspotted', function(req, res) {
   };
 
   spots.push(newSpot);
+  console.log("New pest spotted data is " + spots[spots.length -1]); // TODO check sizeof array syntax
   console.log("Added new location\r\n\r\n");
-  res.send(true);
+  res.send(201, true); // 201 is success resource created
 });
 
+// test curl for authenticating user
+// curl --request POST "localhost:5000/user" --data "user=Matt&password=stuff"
+app.post('/user', function(req,res){
+	console.log("Authenticating user.")
+	if(res.body.user == null || res.body.password == null){
+		console.log("No user or password supplied.");
+		// Challenge Digest scheme is...
+		//   source http://technet.microsoft.com/en-us/library/cc780170%28v=ws.10%29.aspx
+		// Challenge = “Digest” digest-challenge
+		// HTTP Authentication digest-challenge = 1# (
+		//   realm=realm | [domain=“domainname”] |
+		//   nonce=“nonce-value” | [opaque=“opaque-value”] | [stale=(“true” | “false”)] |
+		//   [algorithm=(“MD5” | “MD5-sess” | token] |
+		//   [qop=1#(“auth” | “auth-int” | token)] | [auth-parm]
+		res.set({'WWW-Authenticate' : 'Digest Realm=\"user@judas.heroku.com\"',
+			'qop' : 'auth', // qop Quality Of Protection, auth = authorisation only
+			'nonce' : '12345'}); // nonce, unique encoded value generated for this challenge
+		res.send(401, false); // TODO per wikipedia, need to add a WWW-Authenticate header field to response
+	}
+	if(res.body.user == "Matt" && res.body.password == "stuff"){
+		console.log("Supplied user and password match.");
+		res.send(200, true);
+	} else{
+		console.log("Supplied user and password failed.");
+		res.send(401, false);  // TODO if 401 responce works, repeat here
+	}
+	
+	console.log("Authentication should not get here...");
+	res.send(400, false);
+})
 // end rest
 
 //======================================
