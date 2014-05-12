c449d86dc407044cf7b793f0e619f955f5952090
Matt Stevens <matt@stevens.net.nz>
2014-05-12 18:02:11 +1200

    --

--- server.js
+++ server.js
@@ -113,8 +113,8 @@ app.get('/db', function(req, res){
     console.log("size : " + size);
   });
 
-  res.send("row count is : "+rows.length);
   console.log("size2 : " + rows.length);
+  res.send("row count is : "+rows.length);
 
 //  console.log("row : start of loop");
 //  for(r = 0; r < rows.length; r++){
