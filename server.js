b990bd5de22e45a105e15fab9eaf005cfe74b402
Matt Stevens <matt@stevens.net.nz>
2014-06-11 10:54:49 +1200

    refactor

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ if(authorised(req)){
 
   // build result
     query.on('row', function(row, result){ 
-      rows.push('{pest : '+row.pest+', date : '+row.datestamp+'}');
+      rows.push('{userid : '+row.uid+', pest : '+row.pest+', date : '+row.datestamp+'}');
       console.log('MATT log notes---> added : '+ rows[rows.length-1]);
     });
 
