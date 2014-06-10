f57cdde2e77e25e0a68f7802b481d0e49287bede
Matt Stevens <matt@stevens.net.nz>
2014-06-10 13:35:41 +1200

    working on pestspotted/:date

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/pestspotted/:date', function(req, res){
     nextDay.setDate(nextDay.getDate()+1);
     console.log("MATT log note---> nextDay = "+ nextDay);
 
-    var nextDayStr = ""+nextDay.getFullYear()+"-0"+nextDay.getMonth()+1+"-0"+nextDay.getDate();
+    var nextDayStr = ""+nextDay.getFullYear()+"-0"+(nextDay.getMonth()+1)+"-0"+nextDay.getDate();
     console.log("MATT log note---> nextDayStr = "+ nextDayStr);
 
     var rows = [];
