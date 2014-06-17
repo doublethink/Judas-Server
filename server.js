525a99f7dbe1773e2c9ddffbcb9d35ae8f5522db
Matt-- <matt@stevens.net.nz>
2014-06-17 14:57:52 +1200

    fixing problems

--- server.js
+++ server.js
@@ -248,7 +248,7 @@ if(authorisedAdmin(req)){
 
   // send it back to client
   query.on('end', function(row, result){
-    res.json('{count : ' + count +', reqest: \'get pestspotted/:user/:pest\'}');
+    res.json('{count : ' + count +', reqest: get pestspotted/:user}');
   });
 }
 });
