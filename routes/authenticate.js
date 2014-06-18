98c827466f59f59ac1317c477d5468861cfab0b9
Matt-- <matt@stevens.net.nz>
2014-06-19 00:26:58 +1200

    FB experiment

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -10,7 +10,7 @@
  */
 
 
-
+/*
 var pg =                require('pg')
   , FB =                require('fb')
 //  , Step =              require('step')
@@ -196,7 +196,7 @@ app.post('/user', function(req,res){
   res.send(401, error); // 401 Unauthorized
 });
 */
-
+/*
 exports.setResponse = function(res){
   // Challenge Digest scheme is...
   //   source http://technet.microsoft.com/en-us/library/cc780170%28v=ws.10%29.aspx
@@ -225,3 +225,39 @@ exports.user = function(req){
   res.send(401, "User ID has not been recognised."); // 401 Unauthorized
   return false;
 }
+
+
+// test curl for authenticating user
+// curl --request POST "localhost:5000/user" --data "userId=Matt&password=stuff"
+//app.post('/user', function(req,res){
+/* 
+ref RFC2831 Digest SASL Authentication for steps to implement
+  NB: not a great security protocol, but gets basic securtity in place that can be upgraded later.
+  using qop = 'auth'
+  1. User has not recently authenticated
+  2. User has already authenticated and knows {userId, realm, qop and nonce}
+*/
+/*
+  var error;
+  console.log("Authenticating user.")
+  if(req.body.userId == null || req.body.password == null){
+    error = "No user or password supplied.";
+  }else{
+    for (i in users){
+      //console.log("userId : " + users[i].userId);
+      //console.log("password : " + users[i].password);
+      if(req.body.userId == users[i].userId && req.body.password == users[i].password){
+        var success = "Supplied user and password match.";
+        console.log(success);
+        res.send(200, success);
+			  return;
+      }
+    }
+  }
+
+  var error = error || "Supplied user and password failed.";
+  console.log(error);
+  res = setAuthenticateResponse(res)
+  res.send(401, error); // 401 Unauthorized
+});
+*/
--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.get('/pestspotted/:user',               pests.pestspottedUser);
 //app.get( '/login/callback',                 authenticateFB.loginCallback);
 //app.get( '/logout',                         authenticateFB.logout);
 
+//app.post('/fbtoken_in',                 authenticate.fbtoken_in);
+//app.post('/fbtoken_out',                  authenticate.fbtoken_out);
+
 //=============================
 // tests
 app.get('/test',                        testURI.test);
@@ -64,153 +67,8 @@ app.get('/db/new',                      testURI.dbnew);
 app.get('/db/visits/i',                 testURI.dbvisitsi);
 app.get('/db/visits',                   testURI.dbvisits);
 
-
-//=======================================================================
-// accept and store the FB response token
-// pre-requisite: body contains the Facebook authResponse token
-// NB: not sure this is sufficient to be sure we are communicating with the phone app.
-// tested post content -> {"authResponse" : {"accessToken": "letMeInToo", "expiresIn": "00:01:00", "signedRequest": "signedByMatt", "userID": "Bob"}}
-
-app.post('/fbtoken_in', function(req, res){
-  console.log("MATT log note---> post login");
-
-  // FBtoken is the Facebook authResponse token
-  // https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus/
-  var FBtoken = req.body.authResponse;
-
-if(FBtoken == null){
-  res.send(401, "User token has not been recieved."); // 401 Unauthorized
-} else {
-
-  var insert;
-
-  // create sql INSERT
-  var sql_insert = 'INSERT INTO '+USERDB+
-       '(uid, FBtoken) '+
-       'VALUES ( \''+
-        FBtoken.userID+'\', \''+
-        JSON.stringify(FBtoken) +'\')';
-  console.log('MATT log notes---> sql_insert : '+ sql_insert);
-
-  // add to db
-  client.query(sql_insert);
-  query = client.query('SELECT count(*) FROM '+USERDB);
-
-  // get most recent inserts id based on row count
-  query.on('row', function(row, result){
-    insertId = row.count;
-  });
-
-  // reply to client with id
-  query.on('end', function(row, result){
-    console.log('MATT log notes---> data inserted');
-    console.log('MATT log notes---> result : '+insertId);
-    res.send(201, '{"id" : "'+insertId+'"}');                  // 201 is success resource created
-  });
-}
-});
-
-
-//=====================================================================
-// respond with the Facebook authResponse token
-// pre-requisite: Body contains json {"userID": "xxx"}
-// prerequisite: userID value matches the userID in the token
-// tested post content -> {"userID": "Matt"}
-
-app.post('/fbtoken_out', function(req, res){
-  console.log("MATT log note---> post login");
-
-  var FBuserID = req.body.userID
-    , FBtoken;
-
-if(FBuserID == null){
-  res.send(401, "UserID has not been recieved."); // 401 Unauthorized
-} else {
-
-  var insert;
-
-  // create sql SELECT
-  var sql = 'SELECT * FROM '+USERDB+
-       ' WHERE uid = \''+ FBuserID +'\';';
-  console.log('MATT log notes---> sql : '+ sql);
-
-  // retrieve from db
-  query = client.query(sql);
-
-  // get most recent inserts id based on row count
-  query.on('row', function(row, result){
-    FBtoken = row.fbtoken;  // column name is lowercase out of db
-    console.log('MATT log notes---> FBtoken : '+ FBtoken);
-
-    // little point in checking the userID, but sets up conditional for something stronger
-    if(FBtoken == undefined){
-      return res.send(401, "UserID does not exist in the db."); // 401 Unauthorized
-    }
-    if(FBtoken.userID != FBuserID){
-      return res.send(401, "UserID does not match the stored token."); // 401 Unauthorized
-    }
-  });
-
-  // reply to client with id
-  query.on('end', function(row, result){
-    console.log('MATT log notes---> returning data');
-    res.json(202, FBtoken);                  // 202 request accepted
-  });
-}
-});
-
-
-
-
-
-
-
-
-//======================================
-// restful interfaces
-//======================================
-
-  /*====== POST ======*/
-
-// test curl for authenticating user
-// curl --request POST "localhost:5000/user" --data "userId=Matt&password=stuff"
-app.post('/user', function(req,res){
-/* 
-ref RFC2831 Digest SASL Authentication for steps to implement
-  NB: not a great security protocol, but gets basic securtity in place that can be upgraded later.
-  using qop = 'auth'
-  1. User has not recently authenticated
-  2. User has already authenticated and knows {userId, realm, qop and nonce}
-*/
-  var error;
-  console.log("Authenticating user.")
-  if(req.body.userId == null || req.body.password == null){
-    error = "No user or password supplied.";
-  }else{
-    for (i in users){
-      //console.log("userId : " + users[i].userId);
-      //console.log("password : " + users[i].password);
-      if(req.body.userId == users[i].userId && req.body.password == users[i].password){
-        var success = "Supplied user and password match.";
-        console.log(success);
-        res.send(200, success);
-			  return;
-      }
-    }
-  }
-
-  var error = error || "Supplied user and password failed.";
-  console.log(error);
-  res = setAuthenticateResponse(res)
-  res.send(401, error); // 401 Unauthorized
-});
-// end rest
-
-
-
 //======================================
 // server start
-//======================================
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
