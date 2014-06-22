7679af94e9dfaab010ccda81929495be85b1834b
Matt Stevens <matt@stevens.net.nz>
2014-06-23 11:52:04 +1200

    adding table sorter to report-builder

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -58,7 +58,7 @@ pg.connect(connectionString, function(err, client, done) {
   query.on('row', function(row, result){
      uids.push(row.uid);
   });
-    console.log('MATT log existing uids---> '+uids.tostring());
+    console.log('MATT log existing uids---> '+uids.toString());
   if(FBtoken.uid in uids){
     console.log('MATT log notes---> '+FBtoken.uid+' is already in userdb');
 
