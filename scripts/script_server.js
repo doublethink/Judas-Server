28bc14ff8f3d869c7244c25dcb962708c4e50d98
Matt Stevens <matt@stevens.net.nz>
2014-06-23 18:27:41 +1200

    fixing missing js script

--- scripts/script_server.js
+++ scripts/script_server.js
@@ -5,5 +5,5 @@
 
 exports.serve = function(req, res){
   console.log("MATT log script_server---> " +req.param('script'));
-  res.sendfile('/script/'+req.param('script'));
+  res.sendfile('/scripts/'+req.param('script'));
 };
